1a413fba091e5a032daf93de7da4be62dfb8a939
Scott-fo <scottfo@live.com>
2022-02-04 19:53:56 +0000

    logo change

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,12 +8,8 @@ function Header() {
     const { user } = useMoralis();
   return (
     <div className='sticky top-0 p-5 z-50 bg-black shadow-sm text-pink-500 border-b-2 border-pink-700'>
-        <div className='grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center'>
-            <div className='relative h-24 w-24 mx-auto hidden lg:inline-grid'>
-                <Image layout="fill" src="https://links.papareact.com/3pi" className='rounded-full object-cover' />
-            </div>
-
-            <div className='text-left col-span-4 lg:text-center'>
+        <div className='flex lg:items-center'>
+            <div className='text-left mx-auto lg:text-center'>
                 <div className='relative h-48 w-48 lg:mx-auto border-pink-500 border-8 rounded-full'><Avatar logoutOnPress /></div>
 
                 <h1 className='text-3xl'>Welcome to the metaverse</h1>
